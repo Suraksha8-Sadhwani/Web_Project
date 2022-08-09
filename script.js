7dd6aedc9764c46042d944d6ed80fbaceddddf8f
Suraksha <suraksha.bscsf19@iba-suk.edu.pk>
2022-08-10 00:33:38 +0500

    added the functionality of scroll arrows in the popular songs lists to see the next

--- script.js
+++ script.js
@@ -283,3 +283,16 @@ next.addEventListener('click', () => {
         Array.from(document.getElementsByClassName('songItem'))[`${index+1}`].style.background = 'rgb(105, 105, 170, .1)';
 })
 
+let left_scroll = document.getElementById('left_scroll');
+let right_scroll = document.getElementById('right_scroll');
+let pop_song = document.getElementsByClassName('pop_song')[0];
+
+left_scroll.addEventListener('click',() =>{
+    pop_song.scrollLeft -= 330;
+})
+
+right_scroll.addEventListener('click',() =>{
+    pop_song.scrollLeft += 330;
+})
+
+
